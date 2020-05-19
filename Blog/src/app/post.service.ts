bb83415ef5abe60b344cf819bfbf4a3060fa5e53
Raymon Bye <molinero.gomez.ramon@gmail.com>
2020-05-19 20:40:36 +0200

    Importado Post desde models Creado el array inicializamos en el constructor creamos metodo agregarPost, getAllPost

--- Blog/src/app/post.service.ts
+++ Blog/src/app/post.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { Post } from './models/post.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
+  allPost: Post[];
 
-  constructor() { }
+  constructor() {
+    this.allPost = [
+      {
+        titulo: 'titulo',
+        texto: 'parrafo',
+        autor: 'menganito',
+        imagen: 'url',
+        fecha: '',
+        categoria: '',
+      },
+      {
+        titulo: 'titulo',
+        texto: 'parrafo',
+        autor: 'menganito',
+        imagen: 'url',
+        fecha: '',
+        categoria: '',
+      },
+    ];
+    console.log(this.allPost);
+  }
+
+  agregarPost({
+    titulo,
+    texto,
+    autor,
+    imagen,
+    fecha,
+    categoria,
+  }): Promise<Post[]> {
+    return new Promise((resolve, reject) => {
+      const newPost = new Post(titulo, texto, autor, imagen, fecha, categoria);
+      this.allPost.push(newPost);
+      resolve(this.allPost);
+    });
+  }
+
+  getAllPosts(): Promise<Post[]> {
+    const resolve = new Promise<Post[]>((resolve, reject) => {
+      resolve(this.allPost);
+    });
+    return resolve;
+  }
+
+  /* getPostsByCategoria(cat: string): Promise<Post[]> {
+    return new Promise<Post[]>((resolve, reject)=>{
+      const 
+    })
+  } */
 }
