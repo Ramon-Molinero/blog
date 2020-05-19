87bfc628f6ceeb3f06826b89d7df66fcf6398e66
Raymon Bye <molinero.gomez.ramon@gmail.com>
2020-05-19 20:41:23 +0200

    importado FormsModule y ReactiveForms

--- Blog/src/app/app.module.ts
+++ Blog/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,9 @@ import { BlogComponent } from './blog/blog.component';
 import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BlogComponent,
-    FormularioComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BlogComponent, FormularioComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
