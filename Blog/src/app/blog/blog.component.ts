df4cc1a41629dc068621b0354fba2f8cfe8ab4b6
Raymon Bye <molinero.gomez.ramon@gmail.com>
2020-05-21 18:16:06 +0200

    diseño de la parte Blog, donde se mostrarán los post que creemos

--- Blog/src/app/blog/blog.component.ts
+++ Blog/src/app/blog/blog.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent implements OnInit {
+  allPost: Post[];
 
-  constructor() { }
+  constructor(private postService: PostService) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.allPost = await this.postService.getAllPosts();
   }
-
 }
