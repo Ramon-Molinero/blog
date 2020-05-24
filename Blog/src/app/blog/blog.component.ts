2784652bb57b9de14f8f7121d2b893b00da533f9
Raymon Bye <molinero.gomez.ramon@gmail.com>
2020-05-24 22:45:51 +0200

    Creación de las funciones del blog, mostrar post y filtro por categoria

--- Blog/src/app/blog/blog.component.ts
+++ Blog/src/app/blog/blog.component.ts
@@ -9,10 +9,27 @@ import { Post } from '../models/post.model';
 })
 export class BlogComponent implements OnInit {
   allPost: Post[];
+  postFiltrados: Post[];
 
-  constructor(private postService: PostService) {}
+  constructor(private postService: PostService) {
+    this.allPost = [];
+  }
 
-  async ngOnInit() {
+  ngOnInit() {
+    this.mostrarPosts();
+  }
+
+  async filtrar() {
+    let categoria = (<HTMLInputElement>document.getElementById('selector'))
+      .value;
+    this.allPost = await this.postService.getPostsByCategoria(categoria);
+
+    //console.log(this.postFiltrados);
+  }
+
+  async mostrarPosts() {
     this.allPost = await this.postService.getAllPosts();
+    const reverse = this.allPost.reverse();
+    this.allPost = reverse;
   }
 }
